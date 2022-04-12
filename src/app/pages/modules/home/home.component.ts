d8a5d9f35f9a738447cef1e0792da924c63afdb0
FrancoMelnicov <francogalaxy007@gmail.com>
2022-04-12 15:59:29 -0300

    cambios en el portafolio, colores, diseños, agregando librerias

--- src/app/pages/modules/home/home.component.ts
+++ src/app/pages/modules/home/home.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  listTechnology: Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.listTechnology = [
+      {
+        name: "JavaScript",
+        src: "./assets/images/logos/javascript.png"
+      },
+      {
+        name: "Angular",
+        src: "./assets/images/logos/angular.png"
+      },
+      {
+        name: "Node.js",
+        src: "./assets/images/logos/nodejs.png"
+      },
+      {
+        name: "MongoDB",
+        src: "./assets/images/logos/mongodb.png"
+      },
+      {
+        name: "Bootstrap",
+        src: "./assets/images/logos/bootstrap.png"
+      },
+      {
+        name: "Xampp",
+        src: "./assets/images/logos/xampp.png"
+      }
+    ]
   }
 
 }
